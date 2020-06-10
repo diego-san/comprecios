317a574ec362cad28eabd906ca8caa300e5c966a
diego <diego.san.lan@gmail.com>
2020-06-10 19:38:26 -0400

    primero

--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def inicio(request):
 
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'inicio/index.html')
